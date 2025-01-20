ffd9e217c76b9c51394eb8588fc68b52b7b52840
mohamed elmasry <itsjust4me13@gmail.com>
2025-01-20 12:18:31 +0200

    create `ResponseHelper.php`

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponseHelper
+{
+    public static function success($data = [], $message = 'Success', $code = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public static function error($message = 'Error', $code = 400): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message
+        ], $code);
+    }
+
+    public static function authSuccess($user, $token, $message = 'Success'): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ], 200);
+    }
+}
