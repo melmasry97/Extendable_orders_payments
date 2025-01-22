0142e33d48b299a4079444d9db88ae346f6e5706
mohamed elmasry <itsjust4me13@gmail.com>
2025-01-22 13:05:59 +0200

    fix middleware

--- app/Http/Middleware/ApiAuthenticate.php
+++ app/Http/Middleware/ApiAuthenticate.php
@@ -3,23 +3,26 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use App\Helpers\ResponseHelper;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
 
 class ApiAuthenticate extends Middleware
 {
     /**
-     * Handle an unauthenticated user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  array<string>  $guards
-     * @return void
-     *
-     * @return \Illuminate\Http\JsonResponse
+     * Handle an incoming request.
      */
-    protected function unauthenticated($request, array $guards)
+    public function handle($request, Closure $next, ...$guards)
     {
-        return ResponseHelper::error('Unauthenticated', 401, 'Unauthenticated');
+        try {
+            JWTAuth::parseToken()->authenticate();
+            return $next($request);
+        } catch (JWTException) {
+            return ResponseHelper::error('Unauthenticated', 401);
+        }
     }
 
     /**
